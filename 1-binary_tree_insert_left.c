67ddaf1879dd4f2bac99aaa67723714ced0aa68a
Katemwende <kmwende419@gmail.com>
2023-03-29 12:35:11 +0000

    Set new condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,18 +10,27 @@
   */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *left_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return (NULL);
 	if (parent == NULL)
 		return (NULL);
+	
+	left_node = malloc(sizeof(binary_tree_t));
+	if (left_node == NULL)
+		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
-	new->left = parent->left;
-	new->right = NULL;
+	left_node->n = value;
+	left_node->parent = parent;
+	left_node->right = NULL;
 
-	return (new);
+	if (parent->left == NULL)
+		left_node->left = NULL;
+	else
+	{
+		left_node->left = parent->left;
+		parent->left->parent = left_node;
+	}
+	parent->left = left_node;
+	
+	return (left_node);
 }
